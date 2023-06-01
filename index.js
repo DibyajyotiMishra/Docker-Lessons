581543fd74e21ec9c909681157a55e39aaf6b685
Dibyajyoti Mishra <dibyajyotimishra14@gmail.com>
2023-06-01 11:44:01 +0530

    chore: testing builds from dockerhub

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.use(cors());
 app.enable("trust-proxy");
 
 app.get("/api/v1/", (req, res) => {
-  res.send("<h2>Hi 👋, from Express API within Docker.! </h2>");
+  res.send("<h2>Hi 👋, from Express API within Docker in Production.! </h2>");
 });
 
 app.use("/api/v1/posts", postRouter);
